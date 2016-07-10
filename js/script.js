88fd6c3e69059761e9ca634a4d7ca19379338ede
jasonmziegler <jason@insightfulautomation.com>
2016-07-09 23:16:59 -0500

    Commented out line 3
    Line 3 contained an undeclared variable

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // event listener to respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
-document.getElementById('loadQuote').addEventListener("click", printQuote, false);
+//document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
 //generate a random number 1-6
 var dieRoll = Math.floor(Math.random() * 6) + 1;
