7f520511b97c3e3bb89603c778e2b480921254a1
jasonmziegler <jason@insightfulautomation.com>
2016-07-09 23:12:49 -0500

    Add dieRoll
    Test Random number

--- js/script.js
+++ js/script.js
@@ -2,3 +2,7 @@
 // when user clicks anywhere on the button, the "printQuote" function is called
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
+//generate a random number 1-6
+var dieRoll = Math.floor(Math.random() * 6) + 1;
+//Generate Alert to view random number
+alert('You rolled a ' + dieRoll);
