4de57a864158ef5fefe89047486e436c1bef12cb
jasonmziegler <jason@insightfulautomation.com>
2016-07-10 00:01:42 -0500

    Remove DieRoll add printQuote and message

--- js/script.js
+++ js/script.js
@@ -1,8 +1,26 @@
 // event listener to respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
-//document.getElementById('loadQuote').addEventListener("click", printQuote, false);
+document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
 //generate a random number 1-6
-var dieRoll = Math.floor(Math.random() * 6) + 1;
+// var dieRoll = Math.floor(Math.random() * 6) + 1;
 //Generate Alert to view random number
-alert('You rolled a ' + dieRoll);
+// alert('You rolled a ' + dieRoll);
+
+//create an object
+var quote = {
+  text: 'Mr. Luck says you can change the world.', //text of the quote to display
+  source: 'Mr. Luck', //name of the creator of the quote
+  citation: 'The Tap Room',
+  year: '2003'
+};
+
+//create printQuote function
+function printQuote(quote) {
+  var div = document.getElementById('quote-box');
+  div.innerHTML = message;
+}
+
+//var message = 'Hello';
+
+var message = '<p class="quote">' + quote.text + '</p>' + '<p class="source">' + quote.source + '<span class="citation">' + quote.citation + '</span><span class="year">' + quote.year + '</span></p>';
